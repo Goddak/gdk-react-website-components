f19d3597ed7ce198842f757fec9f6b4aafcaa6ab
Goddak <kris@goddak.co.uk>
2022-12-30 19:23:17 +0000

    Update for NPM Publish

---
+++ dist/index.d.ts
@@ -0,0 +1,21 @@
+import {
+	MenuConfiguration,
+	MenuItem,
+	ResponsiveHeaderProps
+} from "./components/ResponsiveHeader/ResponsiveHeader";
+import {
+	ResponsiveHeroCtaConfig,
+	ResponsiveHeroHeadingConfig,
+	ResponsiveHeroProps,
+	ResponsiveHeroTextConfig
+} from "./components/ResponsiveHero/ResponsiveHero";
+
+export {
+	type MenuConfiguration,
+	type MenuItem,
+	type ResponsiveHeaderProps,
+	type ResponsiveHeroCtaConfig,
+	type ResponsiveHeroHeadingConfig,
+	type ResponsiveHeroProps,
+	type ResponsiveHeroTextConfig
+}
